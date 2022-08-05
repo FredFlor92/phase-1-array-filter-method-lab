a19ec40e35a5297276fd466087631f7739a9ea21
Freddy Florial <freddyflorial@gmail.com>
2022-08-04 20:43:36 -0400

    commit tree 296b167d742cb6329552d0308b31a6e51514f587

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(collection, stringToMatch){
+    
+    const matchList = collection.filter (function (user){
+        return user.toLowerCase() === stringToMatch.toLowerCase();
+    });
+    return matchList;
+}
+function fuzzyMatch(array, string){
+    const matchReturns = array.filter( function (user){
+        return user.startsWith(string);
+    });
+    return matchReturns;
+}
+function matchName(array, string){
+    const matchReturn = array.filter( function (user){
+        return user.name === string;
+    })
+    return matchReturn;
+}
